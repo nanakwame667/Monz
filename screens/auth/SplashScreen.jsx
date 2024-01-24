82abdccb5bcc23f77585a75c6c048725d708053a
Nanakwame <44173438+nanakwame667@users.noreply.github.com>
2024-01-24 01:04:41 +0000

    created a splash screen

--- screens/auth/SplashScreen.jsx
+++ screens/auth/SplashScreen.jsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>Monz</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Monz</Text>
+    </SafeAreaView>
   );
 };
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#052281",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontFamily: "BeVietnamPro_500Medium",
+    fontSize: 48,
+    color: "#fff",
+  },
+});
