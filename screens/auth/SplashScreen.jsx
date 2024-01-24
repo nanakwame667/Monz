e22a5ece94307dca8be86481ba62f24cb296ec45
Nanakwame <44173438+nanakwame667@users.noreply.github.com>
2024-01-24 01:12:39 +0000

    updates

--- screens/auth/SplashScreen.jsx
+++ screens/auth/SplashScreen.jsx
@@ -1,10 +1,30 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const SplashScreen = () => {
+  const [showText, setShowText] = useState(false);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowText(true);
+
+      setTimeout(() => {
+        navigation.navigate("SplashScreen");
+      }, 3000);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [navigation]);
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Monz</Text>
+      <View style={styles.innerContainer}>
+        <Text style={styles.text}>Monz</Text>
+      </View>
+      {showText && <Text style={styles.subText}>Please wait .....</Text>}
     </SafeAreaView>
   );
 };
@@ -18,9 +38,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  innerContainer: {},
   text: {
     fontFamily: "BeVietnamPro_500Medium",
     fontSize: 48,
+    color: "#fff",
+  },
+  subText: {
+    fontFamily: "BeVietnamPro_400Regular",
+    fontSize: 20,
     color: "#fff",
   },
 });
